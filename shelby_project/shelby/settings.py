0ae59d1c6e7d4ec536881eda656a925391067c10
Smex_i_greh <hvatit5minut@gmail.com>
2022-04-10 12:26:29 +0300

    new host

--- shelby_project/shelby/settings.py
+++ shelby_project/shelby/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t2@#9ylyus@awsexdrcftvgbyhnjetrdubhinjilmkmknjiubh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['shelby.pythonanywhere.com']
+ALLOWED_HOSTS = ['Smexigreh.pythonanywhere.com']
 
 
 # Application definition
